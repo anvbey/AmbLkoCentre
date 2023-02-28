0c4556c915381c7b5a88fe11db364577b73d05ae
anvit <anvitgogoa@gmail.com>
2023-03-01 01:21:33 +0530

    firstCommit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Navbar from './Navbar/Navbar';
+import Content from './Content/Content';
+function App() {
+  return (
+    <>
+    <Navbar/> 
+    <Content/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Content/Content.js
@@ -0,0 +1,32 @@
+import { Box, display } from "@mui/system";
+import { Button } from "@mui/material";
+
+const Content= ()=>{
+    return (
+        // Outr box
+        <Box sx={{
+            marginTop: '100px',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'space-evenly'
+        }}>
+
+            {/* Button */}
+            <Box>
+                <Button variant="contained">Vist Us!</Button>
+            </Box>
+
+            {/* images */}
+            <Box>
+            <img
+            src= '/logo192.png'
+            alt= 'blh'
+            loading="lazy"
+          />
+            </Box>
+
+        </Box>
+    )
+}
+
+export default Content;
